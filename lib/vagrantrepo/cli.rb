6be804056148d3fdf2284d740da057c82dae6f67
Jan Vansteenkiste <jan@vstone.eu>
2018-04-27 20:57:28 +0200

    Add options for the collector and collect all information we need

--- lib/vagrantrepo/cli.rb
+++ lib/vagrantrepo/cli.rb
@@ -29,6 +29,16 @@ module Vagrantrepo
 
     def self.options(logger)
       Slop.parse do |o|
+        o.separator 'Box scanner'
+        o.string '--root', 'Repository root directory',
+                 default: Vagrantrepo::Collect::DEFAULT_ROOT
+        o.string '--filter', 'Additional file filter to use.',
+                 default: Vagrantrepo::Collect::DEFAULT_FILTER
+        o.string '--meta-template', 'Naming template for the vagrant meta files.',
+                 default: Vagrantrepo::Collect::DEFAULT_META_TEMPLATE
+        o.string '--url', 'Url repository root.', required: true
+        o.separator ''
+        o.separator 'Global options'
         o.on '-q', '--quiet', 'only warnings and errors are printed.' do
           logger.level = logger.level == Logger::DEBUG ? Logger::DEBUG : Logger::WARN
         end
